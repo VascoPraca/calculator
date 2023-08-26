24ca77827c825598dce8a8c4a6568c3e9ccaf264
VascoPraca <32361310+VascoPraca@users.noreply.github.com>
2023-08-26 19:14:17 +0100

    calculator buttons and basic operation functions created

---
+++ file.js
@@ -0,0 +1,33 @@
+let btn1 = document.querySelector('#btn1'),
+    btn2 = document.querySelector('#btn2'),
+    btn3 = document.querySelector('#btn3'),
+    btn4 = document.querySelector('#btn4'),
+    btn5 = document.querySelector('#btn5'),
+    btn6 = document.querySelector('#btn6'),
+    btn7 = document.querySelector('#btn7'),
+    btn8 = document.querySelector('#btn8'),
+    btn9 = document.querySelector('#btn9'),
+    btn0 = document.querySelector('#btn0');
+
+function operation (num, operator, num2){
+
+}
+
+const add = function (x,y){
+    return x + y;
+}
+const subtract = function (x,y){
+    return x - y;
+}
+const multiply = function (x,y){
+    return x * y;
+}
+const divide = function (x,y){
+    return x / y;
+}
+
+
+function displayNums(){
+    let displayScreen = document.querySelector('#displayScreen');
+    btn1.style.display = displayScreen; 
+}
